c97dbfdcfd4b8959acbf3e2b3937dd2c61bf0e36
Kevin Furlong <kevinmfurlong@gmail.com>
2019-01-29 22:21:51 -0500

    Initial Commit

---
+++ 1/src/app/App.java
@@ -0,0 +1,26 @@
+package app;
+
+/* How Old Are You?
+ * Make a program which displays a different message depending on the age given.
+ * Here are the possible responses:
+ * 
+ * age is less than 16, say "You can't drive."
+ * age is less than 18, say "You can't vote."
+ * age is less than 25, say "You can't rent a car."
+ * age is 25 or over, say "You can do anything that's legal."
+ * 
+ * Here's a sample run. Notice that a person who is under 16 will display three messages, one for being under 16, one for also being under 18, and one for also being under 25.
+ * 
+ * Hey, what's your name? Billy_Corgan
+ * 
+ * Ok, Billy_Corgan, how old are you? 17
+ * 
+ * You can't vote, Billy_Corgan.
+ * You can't rent a car, Billy_Corgan.
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Your code here
+    }
+}
---
+++ 3/src/app/App.java
@@ -0,0 +1,45 @@
+package app;
+
+// A Little Quiz
+// Write an interactive quiz. It should ask the user three multiple-choice or
+// true/false questions about something. It must keep track of how many they
+// get wrong, and print out a "score" at the end.
+
+// Are you ready for a quiz?  N
+// Okay, here it comes!
+
+// Q1) What is the capital of Alaska?
+// 	1) Melbourne
+// 	2) Anchorage
+// 	3) Juneau
+//
+// > 3
+//
+// That's right!
+//
+// Q2) Can you store the value "cat" in a variable of type int?
+// 	1) yes
+// 	2) no
+//
+// > 1
+//
+// Sorry, "cat" is a string. ints can only store numbers.
+//
+// Q3) What is the result of 9+6/3?
+// 	1) 5
+// 	2) 11
+// 	3) 15/3
+//
+// > 2
+//
+// That's correct!
+//
+//
+// Overall, you got 2 out of 3 correct.
+// Thanks for playing!
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Your code here
+    }
+}
